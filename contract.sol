b2e51f486dd770b29cdb44cef715fde6d17ff4e1
Marat Askarbek <96424646+marat0s@users.noreply.github.com>
2023-10-06 16:07:43 +0600

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -2,79 +2,76 @@
 pragma solidity ^0.8.0;
 
 contract RockPaperScissors {
+    enum Move { None, Rock, Paper, Scissors }
+    struct Player {
+        bytes32 encryptedMove;
+        Move move;
+    }
+
     address public player1;
     address public player2;
-    bytes32 public player1EncryptedMove;
-    bytes32 public player2EncryptedMove;
-    string public player1Move;
-    string public player2Move;
-    uint256 public player1Bet;
-    uint256 public player2Bet;
-    uint256 public constant MIN_BET = 0.0001 ether;
-    uint256 public constant REVEAL_TIMEOUT = 1 hours; // or any desired time
+    mapping(address => Player) public players;
+    uint256 public betAmount = 0.0001 ether; // tBNB is equivalent to ether in BSC
+    uint256 public REVEAL_TIMEOUT = 3 minutes;
     uint256 public revealDeadline;
 
-    enum GameState { WaitingForPlayers, WaitingForMoves, WaitingForReveal, GameOver }
-    GameState public state = GameState.WaitingForPlayers;
-
-    modifier inState(GameState _state) {
-        require(state == _state, "Invalid game state.");
-        _;
-    }
-
     modifier onlyPlayers() {
-        require(msg.sender == player1 || msg.sender == player2, "Only registered players can call this.");
+        require(msg.sender == player1 || msg.sender == player2, "Not a player");
         _;
     }
 
-    function register() external payable inState(GameState.WaitingForPlayers) {
-        require(msg.value >= MIN_BET, "Bet amount is less than minimum required.");
+    function register() external payable {
+        require(player1 == address(0) || player2 == address(0), "Already two players");
+        require(msg.value == betAmount, "Incorrect bet amount");
+
         if (player1 == address(0)) {
             player1 = msg.sender;
-            player1Bet = msg.value;
-        } else if (player2 == address(0) && msg.sender != player1) {
+        } else {
             player2 = msg.sender;
-            player2Bet = msg.value;
-            state = GameState.WaitingForMoves;
-        } else {
-            revert("Invalid registration.");
-        }
-    }
-
-    function play(bytes32 encrMove) external onlyPlayers inState(GameState.WaitingForMoves) {
-        if (msg.sender == player1) {
-            player1EncryptedMove = encrMove;
-        } else {
-            player2EncryptedMove = encrMove;
-        }
-
-        if (player1EncryptedMove != bytes32(0) && player2EncryptedMove != bytes32(0)) {
-            state = GameState.WaitingForReveal;
             revealDeadline = block.timestamp + REVEAL_TIMEOUT;
         }
     }
 
-    function reveal(string memory clearMove, string memory password) external onlyPlayers inState(GameState.WaitingForReveal) {
-        require(block.timestamp <= revealDeadline, "Reveal time is over.");
-        bytes32 moveHash = keccak256(abi.encodePacked(clearMove, password));
-        if (msg.sender == player1 && moveHash == player1EncryptedMove) {
-            player1Move = clearMove;
-        } else if (msg.sender == player2 && moveHash == player2EncryptedMove) {
-            player2Move = clearMove;
-        } else {
-            revert("Invalid move or password.");
-        }
+    function play(bytes32 _encryptedMove) external onlyPlayers {
+        require(players[msg.sender].encryptedMove == bytes32(0), "Move already made");
+        players[msg.sender].encryptedMove = _encryptedMove;
+    }
 
-        if (bytes(player1Move).length > 0 && bytes(player2Move).length > 0) {
-            state = GameState.GameOver;
-            // Determine winner and distribute rewards
-            getOutcome();
+    function reveal(Move _move, string memory _password) external onlyPlayers {
+        require(block.timestamp <= revealDeadline, "Reveal phase is over");
+        require(players[msg.sender].move == Move.None, "Move already revealed");
+        require(keccak256(abi.encodePacked(_move, _password)) == players[msg.sender].encryptedMove, "Invalid move or password");
+
+        players[msg.sender].move = _move;
+
+        if (players[player1].move != Move.None && players[player2].move != Move.None) {
+            determineWinner();
         }
     }
 
-    function getOutcome() internal inState(GameState.GameOver) {
-        // Logic to determine winner based on player1Move and player2Move
-        // Distribute rewards accordingly
+    function determineWinner() internal {
+        Move player1Move = players[player1].move;
+        Move player2Move = players[player2].move;
+
+        if (player1Move == player2Move) {
+            payable(player1).transfer(betAmount);
+            payable(player2).transfer(betAmount);
+        } else if ((player1Move == Move.Rock && player2Move == Move.Scissors) ||
+                   (player1Move == Move.Paper && player2Move == Move.Rock) ||
+                   (player1Move == Move.Scissors && player2Move == Move.Paper)) {
+            payable(player1).transfer(betAmount * 2);
+        } else {
+            payable(player2).transfer(betAmount * 2);
+        }
+
+        resetGame();
+    }
+
+    function resetGame() internal {
+        delete players[player1];
+        delete players[player2];
+        delete player1;
+        delete player2;
     }
 
     function getContractBalance() external view returns (uint256) {
@@ -88,11 +85,11 @@ contract RockPaperScissors {
     }
 
     function bothPlayed() external view returns (bool) {
-        return player1EncryptedMove != bytes32(0) && player2EncryptedMove != bytes32(0);
+        return players[player1].encryptedMove != bytes32(0) && players[player2].encryptedMove != bytes32(0);
     }
 
     function bothRevealed() external view returns (bool) {
-        return bytes(player1Move).length > 0 && bytes(player2Move).length > 0;
+        return players[player1].move != Move.None && players[player2].move != Move.None;
     }
 
     function revealTimeLeft() external view returns (uint256) {
